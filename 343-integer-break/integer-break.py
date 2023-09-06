cc887c52435451f48e0de796e6b988cf803d203f
Shuwen <jacobc3@gmail.com>
2023-09-06 16:09:59 -0700

    Time: 39 ms (74.55%) | Memory: 16.2 MB (70.69%) - LeetSync

--- 343-integer-break/integer-break.py
+++ 343-integer-break/integer-break.py
@@ -1,7 +1,18 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
         dp = {}
-        
+        dp[1] = 1
+        dp[2] = 2
+        for num in range(2, n+1):
+            dp[num] = 0 if num == n else num
+            for i in range(1, num//2+1):
+                product = dp[num-i] * dp[i]
+                dp[num]  = max(dp[num], product)
+        return dp[n]
+
+
+
+
         def dfs(num):
             if num in dp:
                 return dp[num]
