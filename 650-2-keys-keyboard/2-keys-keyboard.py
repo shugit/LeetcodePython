77543c6c16f00b1b3a817e2b8fdac1f1f69f122f
Shuwen <jacobc3@gmail.com>
2023-09-06 16:25:13 -0700

    Time: 509 ms (18.16%) | Memory: 16.4 MB (41.50%) - LeetSync

---
+++ 650-2-keys-keyboard/2-keys-keyboard.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+        dp = {}
+        dp[0] = 0
+        dp[1] = 0
+
+        for i in range(2, n+1): 
+            dp[i] = i
+            for j in range(2,i):
+                if i % j == 0:
+                    dp[i] = min(dp[j] + i//j, dp[i])
+
+        return dp[n]
