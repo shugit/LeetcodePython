211a4480c47369cf571fee20f0e2a03171ca036c
Shuwen <jacobc3@gmail.com>
2023-09-08 14:42:42 -0700

    Time: 693 ms (67.07%) | Memory: 16.4 MB (88.43%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -4,18 +4,19 @@ class Solution:
         if s % 2 != 0:
             return False
         target = s // 2
-        dp = [ [False for _ in range(target+1)] for _ in range(len(nums))]
-        if nums[0] <= target:
-            dp[0][nums[0]] = True
+        dp = [False for _ in range(target+1)] 
+        dp[0] = True
+        # if nums[0] <= target:
+        #     dp[nums[0]] = True
 
-        for i in range(len(nums)):
-            for j in range(target+1):
-                dp[i][j] = dp[i-1][j]
-                if nums[i] == j:
-                    dp[i][j] = True
-                    continue
-                if nums[i] < j :
-                    dp[i][j] = dp[i-1][j] or dp[i-1][j-nums[i]]
-            if dp[i][target] == True:
+        for i in range(1, len(nums)):
+            curr = nums[i-1]
+            for j in range(target, -1,-1):
+                if curr <= j :
+                    dp[j] = dp[j] or dp[j-curr]
+                    # print(i,j,dp)
+            # print(curr,dp)
+            if dp[target] == True:
+                # print(i,j)
                 return True
-        return dp[len(nums)-1][target]
+        return dp[target]
