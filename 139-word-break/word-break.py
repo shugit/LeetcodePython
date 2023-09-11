3ad56abe1abc5bed3c61250f2df0fd46c5097b9e
Shuwen <jacobc3@gmail.com>
2023-09-10 20:31:34 -0700

    Time: 54 ms (17.64%) | Memory: 16.4 MB (82.78%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -5,10 +5,9 @@ class Solution:
         dp[0] = True
         for i in range(1, n+1):
             for j in range(0,i):
-                cur = s[j:i] 
+                cur = s[j:i] #实际取到的是j~i-1，因为i从1开始，j从0开始，所以正好
+                dp[i] = dp[j] and cur in wordDict
                 if dp[i] is True:
                     break
-                else:
-                    dp[i] = dp[j] and cur in wordDict
 
         return dp[n]
