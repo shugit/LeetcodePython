283385d540524e4a0bd7b2a100099838c82861f7
Shuwen <jacobc3@gmail.com>
2023-09-11 14:39:11 -0700

    Time: 185 ms (90.76%) | Memory: 24.3 MB (38.60%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -19,7 +19,7 @@ class Solution:
         res = [1] * n
         for i in range(1,n):
             res[i] = res[i-1] * nums[i-1]
-        print(res)
+        # print(res)
         post = 1
         for i in range(n-2, -1, -1):
             post = post * nums[i+1]
