42b40f2b235bb21f7f0f050f094f843986dccc98
Shuwen <jacobc3@gmail.com>
2023-09-10 21:25:33 -0700

    Time: 47 ms (50.84%) | Memory: 16.4 MB (37.15%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.py
+++ 377-combination-sum-iv/combination-sum-iv.py
@@ -10,16 +10,3 @@ class Solution:
                 if j-cur >=0:
                     dp[j] += dp[j-cur]
         return dp[target]
-
-    def combinationSum5(self, nums: List[int], target: int) -> int:
-        size = len(nums)
-        dp = [0 for _ in range(target + 1)]
-        dp[0] = 1
-
-        for w in range(target + 1):
-            for i in range(1, size + 1):
-                if w >= nums[i - 1]:
-                    dp[w] = dp[w] + dp[w - nums[i - 1]]
-            print(dp)
-            
-        return dp[target]
