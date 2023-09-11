1f1a93e9b8670678b78f12399440438d3c9b3409
Shuwen <jacobc3@gmail.com>
2023-09-10 20:30:48 -0700

    Time: 53 ms (20.39%) | Memory: 16.4 MB (82.78%) - LeetSync

---
+++ 139-word-break/word-break.py
@@ -0,0 +1,14 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        dp = [False for _ in range(n+1)]
+        dp[0] = True
+        for i in range(1, n+1):
+            for j in range(0,i):
+                cur = s[j:i] #实际取到的是j~i-1，因为i从1开始，j从0开始，所以正好
+                if dp[i] is True:
+                    break
+                else:
+                    dp[i] = dp[j] and cur in wordDict
+
+        return dp[n]
