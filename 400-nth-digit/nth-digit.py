2b164d5bdb475478933943c5a68d6af1bb7624fb
Shuwen Zhou <jacobc3@gmail.com>
2023-11-07 16:10:38 -0800

    Time: 26 ms (98.39%) | Memory: 16.2 MB (70.86%) - LeetSync

--- 400-nth-digit/nth-digit.py
+++ 400-nth-digit/nth-digit.py
@@ -1,12 +1,10 @@
 class Solution:
     def findNthDigit(self, n: int) -> int:
-        digit = 1
-        count = 9
-        while count * digit < n:
-            n -= count * digit
+        base = digit = 1
+        while n > 9 * base * digit:
+            n -= 9 * base * digit
             digit += 1
-            count *= 10
-        number = (count / 9) + (n-1) / digit
-        index = (n-1) % digit
-        return int(str(number)[index])
+            base *= 10
+        q, r = divmod(n-1, digit)
+        return int(str(base+q)[r])
         
