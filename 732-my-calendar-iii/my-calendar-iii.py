93da72bb7876fbfd70388f2ff615c7f93ea10947
Shuwen Zhou <jacobc3@gmail.com>
2023-11-29 12:19:36 -0800

    Time: 677 ms (82.59%) | Memory: 17.3 MB (10.93%) - LeetSync

--- 732-my-calendar-iii/my-calendar-iii.py
+++ 732-my-calendar-iii/my-calendar-iii.py
@@ -7,12 +7,13 @@ class MyCalendarThree:
     def book(self, startTime: int, endTime: int) -> int:
         self.d[startTime] = self.d.get(startTime, 0) + 1
         self.d[endTime] = self.d.get(endTime,0) - 1
-        prefix = 0
-        cnt = 0
-        for key, value in self.d.items():
-            prefix += value
-            cnt = max(cnt, prefix)
-        return cnt
+        # prefix = 0
+        # cnt = 0
+        # for key, value in self.d.items():
+        #     prefix += value
+        #     cnt = max(cnt, prefix)
+        # return cnt
+        return max(accumulate(self.d.values()))
 
 
 # Your MyCalendarThree object will be instantiated and called as such:
