417558a2bc7ca7a23bd7d7a7f59f502955a20347
Shuwen Zhou <jacobc3@gmail.com>
2023-11-29 11:07:52 -0800

    Time: 784 ms (16.93%) | Memory: 30.3 MB (73.14%) - LeetSync

---
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.py
@@ -0,0 +1,10 @@
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        arr = [0] * (n+1)
+        for start,end, num in bookings:
+            arr[start-1] += num
+            arr[end + 1 - 1] -= num
+        for i in range(1, n):
+            arr[i] += arr[i-1]
+
+        return arr[0:n]
