1dbe6917c09f3629f1e39e8a3d9771ad5b66406a
Shuwen Zhou <jacobc3@gmail.com>
2023-11-29 13:02:19 -0800

    Time: 42 ms (91.10%) | Memory: 16.4 MB (20.87%) - LeetSync

--- 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.py
+++ 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.py
@@ -1,7 +1,7 @@
 class Solution:
     def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
         ranges.sort(key=lambda x: (x[0], x[1]))
-        print(ranges)
+        # print(ranges)
         for i in range(left, right + 1):
             inside = False
             for x,y in ranges:
