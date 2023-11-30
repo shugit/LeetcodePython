4e3399bc997d4303cc23553cf8f7a596c518dce5
Shuwen Zhou <jacobc3@gmail.com>
2023-11-30 14:09:32 -0800

    Time: 1278 ms (93.76%) | Memory: 33.3 MB (31.82%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -16,13 +16,19 @@ class Solution:
         return res
 
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        q = []
+        q = deque()
+        res = []
         for i in range(0,k):
-            heapq.heappush(q, (-nums[i], i)) #q = [-val, i]
-        arr= [-q[0][0]]
+            while q and q[-1] < nums[i]:
+                q.pop()
+            q.append(nums[i])
+        res.append(q[0])
         for i in range(k, len(nums)):
-            while q and q[0][1] <= i - k:
-                heapq.heappop(q)
-            heapq.heappush(q, (-nums[i], i))
-            arr.append(-q[0][0])
-        return arr
+            while q and q[-1] < nums[i]:
+                q.pop()
+            q.append(nums[i])
+            if q[0] == nums[i-k]:
+                q.popleft()
+            res.append(q[0])
+        return res
+            
