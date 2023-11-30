30f55c83667aaade668eb645c852de9ef4a76f93
Shuwen Zhou <jacobc3@gmail.com>
2023-11-30 15:12:19 -0800

    Time: 85 ms (82.37%) | Memory: 16.7 MB (20.68%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,20 +1,22 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        p1, p2 = 0,0
-        arr = []
-        while p1 < len(nums1) and p2 < len(nums2):
-            if nums1[p1] < nums2[p2]:
-                arr.append(nums1[p1])
-                p1 += 1
-            else:
-                arr.append(nums2[p2])
-                p2 += 1
-        while p1 < len(nums1):
-            arr.append(nums1[p1])
-            p1 += 1
-        while p2 < len(nums2):
-            arr.append(nums2[p2])
-            p2 += 1
+        # p1, p2 = 0,0
+        # arr = []
+        # while p1 < len(nums1) and p2 < len(nums2):
+        #     if nums1[p1] < nums2[p2]:
+        #         arr.append(nums1[p1])
+        #         p1 += 1
+        #     else:
+        #         arr.append(nums2[p2])
+        #         p2 += 1
+        # while p1 < len(nums1):
+        #     arr.append(nums1[p1])
+        #     p1 += 1
+        # while p2 < len(nums2):
+        #     arr.append(nums2[p2])
+        #     p2 += 1
+        arr = nums1 + nums2
+        arr.sort()
         n = len(arr)
         if  n % 2 == 1:
             return arr[n//2] 
