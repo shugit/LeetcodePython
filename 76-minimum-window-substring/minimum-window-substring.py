0e8a85478e7eba02024f8409ed20cd8c914ed0e2
Shuwen Zhou <jacobc3@gmail.com>
2023-11-30 15:02:20 -0800

    Time: 102 ms (80.15%) | Memory: 17.2 MB (11.01%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        window = {}
+        count = {}
+        for c in t:
+            count[c] = count.get(c, 0) + 1
+        matched = 0
+        res = []
+        mini = inf
+        l = 0
+        for r in range(0, len(s)):
+            c = s[r]
+            window[c] = window.get(c,0) + 1
+            if c in count and window[c] == count[c]:
+                matched += 1
+            while matched == len(count):
+                if (r-l+1) < mini:
+                    res = [l,r]
+                    mini = r - l + 1
+                window[s[l]] -= 1
+                if s[l] in count and window[s[l]] < count[s[l]]:
+                    matched -= 1
+                l += 1
+        return s[res[0]:res[1] + 1] if mini != inf else ""
