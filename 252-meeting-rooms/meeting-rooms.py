2c2437645e65ea5bc7b035dab97c50365341cd4f
Shuwen Zhou <jacobc3@gmail.com>
2023-12-01 12:27:56 -0800

    Time: 87 ms (16.47%) | Memory: 19.8 MB (40.12%) - LeetSync

---
+++ 252-meeting-rooms/meeting-rooms.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        intervals.sort()
+        if not intervals:
+            return True
+        s, e = intervals[0]
+        for i, (left, right) in enumerate(intervals[1:]):
+            if e <= left:
+                s,e = left, right
+                continue
+            else:
+                return False
+        return True
