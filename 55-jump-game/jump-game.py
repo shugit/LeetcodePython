af9d23eff058da570f227b67cccc85db3d3aeb67
Shuwen Zhou <jacobc3@gmail.com>
2023-12-01 14:38:44 -0800

    Time: 6665 ms (5.20%) | Memory: 17.5 MB (72.90%) - LeetSync

---
+++ 55-jump-game/jump-game.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        dp = [False] * len(nums)
+        dp[len(nums)-1] = True
+        for i in range(len(nums)-2, -1, -1):
+            if nums[i] + i >= len(nums):
+                dp[i] = True
+            else:
+                for step in range(1, nums[i]+1):
+                    if dp[i + step] == True:
+                        dp[i] = True
+        return dp[0]
