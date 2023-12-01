9f762fe9a7536ce15846c469e53b8b181f52d7f0
Shuwen Zhou <jacobc3@gmail.com>
2023-11-30 21:29:52 -0800

    Time: 391 ms (91.53%) | Memory: 17 MB (5.04%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,5 +1,5 @@
 class Solution:
-    def leastInterval(self, tasks: List[str], n: int) -> int:
+    def leastInterval2(self, tasks: List[str], n: int) -> int:
         # frequencies of the tasks
         freq = Counter(tasks)
         max_freq = max(freq.values())
@@ -10,3 +10,18 @@ class Solution:
                 max_freq_count += 1
         res = (max_freq-1) * (n+1) + max_freq_count
         return max(res, len(tasks))
+
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        # frequencies of the tasks
+        frequencies = Counter(tasks)
+        q = sorted(frequencies.values())
+
+        # max frequency
+        f_max = q.pop()
+        idle_time = (f_max - 1) * n
+        
+        while q and idle_time > 0:
+            idle_time -= min(f_max - 1, q.pop())
+        idle_time = max(0, idle_time)
+
+        return idle_time + len(tasks)
