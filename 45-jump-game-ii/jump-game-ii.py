9acc9456b947509f59cea965bec3b99c06637993
Shuwen Zhou <jacobc3@gmail.com>
2023-12-01 15:25:23 -0800

    Time: 111 ms (89.75%) | Memory: 17.5 MB (26.76%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -1,5 +1,5 @@
 class Solution:
-    def jump(self, nums: List[int]) -> int:
+    def jump_dp(self, nums: List[int]) -> int:
         n = len(nums)
         dp = [inf] * n
         dp[n-1] = 0
@@ -8,10 +8,18 @@ class Solution:
                 dp[i] = 1
                 continue
             for step in range(1, min(nums[i] + 1, n - i)):
-                # if i + step <= n:
-                    dp[i] = min(dp[i],  dp[ i + step ] + 1)
+                dp[i] = min(dp[i],  dp[ i + step ] + 1)
         return dp[0]
 
 
-    def jump_greedy(self, nums: List[int]) -> int:
-        return 0
+    def jump(self, nums: List[int]) -> int:
+        l,r = 0,0
+        res = 0
+        while r < len(nums) - 1:
+            far = 0
+            for i in range(l, r+1):
+                far = max(far, i + nums[i])
+            l = r + 1
+            r = far
+            res += 1
+        return res
