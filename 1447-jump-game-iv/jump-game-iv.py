9ad43c59db2779595e3c86ac9454c98cc4f32975
Shuwen Zhou <jacobc3@gmail.com>
2023-12-01 15:53:10 -0800

    Time: 669 ms (36.15%) | Memory: 31.6 MB (46.04%) - LeetSync

---
+++ 1447-jump-game-iv/jump-game-iv.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minJumps(self, arr: List[int]) -> int:
+        g = {}
+        for i, a in  enumerate(arr):
+            if a not in g:
+                g[a] = []
+            g[a].append(i)
+        step = 0
+        q = [0]
+        visited = set([0])
+        # visited.add(0)
+        # print(g)
+        while q:
+            level = []
+            for i in q:
+                if i == len(arr) - 1:
+                    return step
+                for next_i in g[arr[i]]:
+                    if next_i not in visited:
+                        visited.add(next_i)
+                        level.append(next_i)
+                g[arr[i]].clear()
+                if i - 1 >= 0 and i-1 not in visited:
+                    visited.add(i-1)
+                    level.append(i-1)
+                if i + 1 <= len(arr) - 1 and i+1 not in visited:
+                    visited.add(i+1)
+                    level.append(i+1)
+            step += 1
+            print(step, level)
+            q = level
+        return -1
