446d0e176896a9843f5597cb582c6496098b5eff
Shuwen Zhou <jacobc3@gmail.com>
2023-12-01 12:10:07 -0800

    Time: 159 ms (5.16%) | Memory: 21.4 MB (13.87%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -7,10 +7,8 @@ class Solution:
             if e < left:
                 res.append([s,e])
                 s, e = left, right
-            # elif s > right:
-                # res.append([left,right])
             else:
-                s = min(left, s)
+                # s = min(left, s)
                 e = max(right, e)
         res.append([s,e])
         return res
