b07c6315660f81d0f2907449d300a78b0e708e95
Shuwen Zhou <jacobc3@gmail.com>
2023-12-01 11:52:47 -0800

    Time: 78 ms (82.31%) | Memory: 19.8 MB (25.62%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -3,9 +3,7 @@ class Solution:
         s = newInterval[0]
         e = newInterval[1]
         res = []
-        for i in range(0,len(intervals)):
-            left = intervals[i][0]
-            right = intervals[i][1]
+        for i,(left,right) in enumerate(intervals):
             if e < left:
                 res.append([s,e])
                 return res + intervals[i:]
