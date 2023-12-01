a43aa0013b3928d7709b0abb81190a20fe6dfe57
Shuwen Zhou <jacobc3@gmail.com>
2023-11-30 16:39:57 -0800

    Time: 55 ms (73.35%) | Memory: 16.8 MB (30.35%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,28 +1,26 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        col=[]
-        posdiag=[]
-        negdiag=[]
-        ans=[]
-        board=[['.']*n for i in range(n)]
-        def backtrack(r):
-            if r==n:
-                l=["".join(i) for i in board]
-                ans.append(l)
+        res = []
+        board = [['.']*n for _ in range(n)]
+        col = set()
+        diag = set()
+        r_diag = set()
+        def bt(i):
+            if i == n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
                 return
-            for c in range(n):
-                if c in col or (r+c) in posdiag or (r-c) in negdiag:
+            for j in range(n):
+                if j in col or (i+j) in diag or (i-j) in r_diag:
                     continue
-                board[r][c]="Q"
-                col.append(c)
-                posdiag.append(r+c)
-                negdiag.append(r-c)
-
-                backtrack(r+1)
-
-                board[r][c]="."
-                col.remove(c)
-                posdiag.remove(r+c)
-                negdiag.remove(r-c)
-        backtrack(0)
-        return ans
+                col.add(j)
+                diag.add(i+j)
+                r_diag.add(i-j)
+                board[i][j] = "Q"
+                bt(i+1)
+                col.remove(j)
+                diag.remove(i+j)
+                r_diag.remove(i-j)
+                board[i][j] = '.'
+        bt(0)
+        return res
