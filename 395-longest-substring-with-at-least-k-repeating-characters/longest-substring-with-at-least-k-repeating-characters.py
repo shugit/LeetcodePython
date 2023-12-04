def0f2416ea1c79029ae3dcf72b7f68809c85604
Shuwen Zhou <jacobc3@gmail.com>
2023-12-03 21:58:05 -0800

    Time: 831 ms (15.65%) | Memory: 64 MB (7.56%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
@@ -1,11 +1,14 @@
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
-        c = Counter(s)
-        left = 0
-        maxi = 0
-        for i, letter in enumerate(s):
-            if c[letter] < k:
-                left = self.longestSubstring(s[:i], k)
-                right = self.longestSubstring(s[i+1:], k)
-                return max(left, right)
-        return len(s)
+
+        def find(s):
+            if len(s) < k:
+                return 0
+            c = Counter(s)
+            for i, letter in enumerate(s):
+                if c[letter] < k:
+                    left = find(s[:i])
+                    right = find(s[i+1:])
+                    return max(left, right)
+            return len(s)
+        return find(s)
