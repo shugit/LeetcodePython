20e16dabfb09d7cf267e5d128fddbd457c17fb08
Shuwen Zhou <jacobc3@gmail.com>
2023-12-03 20:16:56 -0800

    Time: 144 ms (41.44%) | Memory: 17.5 MB (47.21%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,5 +1,5 @@
 class Solution:
-    def findAnagrams(self, s: str, p: str) -> List[int]:
+    def findAnagrams2(self, s: str, p: str) -> List[int]:
         cp = Counter(p)
         cs = Counter(s[0:len(p)])
         res = []
@@ -11,4 +11,25 @@ class Solution:
             if cp == cs:
                 res.append(i)
         return res
-            
+
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        p_counter = Counter(p)
+        s_counter = Counter()
+        left,right = 0,0
+        matched = 0
+        res = []
+        while right < len(s):
+            c = s[right]
+            s_counter[c] += 1
+            if s_counter[c] == p_counter[c]:
+                matched += 1
+            while matched == len(p_counter) and right - left + 1 > len(p):
+                left_c = s[left]
+                s_counter[left_c] -= 1
+                if s_counter[left_c] < p_counter[left_c]:
+                    matched -= 1
+                left += 1
+            if right-left + 1 == len(p) and matched == len(p_counter):
+                res.append(left)
+            right += 1
+        return res
