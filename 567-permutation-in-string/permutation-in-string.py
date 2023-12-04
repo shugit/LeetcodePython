47a11e97fd0183dde606a18b62f74570588a5c87
Shuwen Zhou <jacobc3@gmail.com>
2023-12-03 19:41:42 -0800

    Time: 93 ms (38.37%) | Memory: 16.5 MB (9.60%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,37 +1,24 @@
 class Solution:
-    def checkInclusion2(self, s1: str, s2: str) -> bool:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
             return False
-        h1 = {}
-        h2 = {}
-        for i in range(0, 26):
-            c = chr(ord("a") + i)
-            h1[c] = 0
-            h2[c] = 0
-        for s in s1:
-            h1[s] += 1
+        h1 = Counter(s1)
+        h2 = Counter()
         i = 0
         n = len(s1)
         for i in range(0, n):
             h2[s2[i]] += 1
-        def match(h1,h2):
-            # print(h1,h2)
-            for i in range(0, 26):
-                c = chr(ord("a") + i)
-                if h1[c] != h2[c]:
-                    return False
-            return True
-        if match(h1,h2):
+        if h1 == h2:
             return True
         for i in range(1, len(s2) - len(s1) + 1):
             j = i + len(s1) - 1
             h2[s2[i-1]] -= 1
             h2[s2[j]] += 1
-            if match(h1,h2):
+            if h1 == h2:
                 return True
         return False
         
-    def checkInclusion(self, s1: str, s2: str) -> bool:
+    def checkInclusion2(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
             return False
         c1 = Counter(s1)
@@ -51,6 +38,5 @@ class Solution:
                     matched -= 1
             if matched == len(c1) and right - left + 1 == len(s1):
                 return True
-
         return False
             
