fb02c668b4b76d4e05d0795c521f7e3bb3294e4a
Shuwen Zhou <jacobc3@gmail.com>
2023-12-10 19:57:26 -0700

    Time: 72 ms (90.16%) | Memory: 16.5 MB (52.94%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.py
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.py
@@ -1,30 +1,31 @@
 class Solution:
-    def canPartitionKSubsets2   (self, nums: List[int], k: int) -> bool:
+    def canPartitionKSubsets (self, nums: List[int], k: int) -> bool:
         tsum = sum(nums)
         target = tsum // k
         if tsum / k != target:
             return False
         nums.sort(reverse=True)
-        used = [False] * len(nums)
+        used = set()
         # found = False
-        def bt(i, k, subsetSum):
-            if k == 0:
+        sums = [0]*k
+        def bt(i):
+            if i == len(nums):
                 return True
-            if subsetSum == target:
-                return bt(0, k-1, 0)
-            for j in range(i, len(nums)):
-                if i > 0 and not used[i-1] and nums[i] == nums[i-1]:
+            for j in range(0, k):
+                n = nums[i]
+                if n + sums[j] > target:
                     continue
-                if used[j] or subsetSum + nums[j] > target:
-                    continue
-                used[j] = True 
-                if bt(j + 1, k, subsetSum + nums[j]):
+                sums[j] += n
+                if bt(i+1):
                     return True
-                used[j] = False
+                sums[j] -= n
+                if sums[j] == 0:
+                    break
+            # print(i,k,subsetSum,"False")
             return False
-        return bt(0, k, 0)
+        return bt(0)
 
-    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+    def canPartitionKSubsets2(self, nums: List[int], k: int) -> bool:
         total = sum(nums)
 
         if total % k:
