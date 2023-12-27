3c8b9328235fe4438a8b7c9c50ddcd0bdd4a05a8
Shuwen Zhou <jacobc3@gmail.com>
2023-12-27 10:32:12 -0800

    Time: 215 ms (7.30%) | Memory: 18.1 MB (9.72%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.py
+++ 794-swim-in-rising-water/swim-in-rising-water.py
@@ -1,18 +1,21 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
+        if m == n == 1:
+            return grid[0][0]
         res = grid[m-1][n-1]
         q = [(grid[0][0],0,0)]
         visited = set()
         while q:
             h,i,j = heapq.heappop(q)
+            res = max(h, res)
             if i == m-1 and j == n-1:
-                return h
+                return res
             if (i,j) in visited:
                 continue
             visited.add((i,j))
             for stepI, stepJ in [[1,0],[-1,0],[0,1],[0,-1]]:
                 newI,newJ = stepI+i, stepJ+j
                 if 0 <= newI < m and 0 <= newJ < n and (newI, newJ) not in visited:
-                    heapq.heappush(q, [max(h, grid[newI][newJ]), newI, newJ])
-        return -1
+                    heapq.heappush(q, [grid[newI][newJ], newI, newJ])
+        return res
