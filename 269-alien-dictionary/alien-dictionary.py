78c7edc07964e82bec5dad6b185b18c40b74f77e
Shuwen Zhou <jacobc3@gmail.com>
2023-12-28 12:23:13 -0800

    Time: 79 ms (5.62%) | Memory: 17.5 MB (7.13%) - LeetSync

---
+++ 269-alien-dictionary/alien-dictionary.py
@@ -0,0 +1,28 @@
+class Solution:
+    def alienOrder(self, words: List[str]) -> str:
+        g = {c:set() for w in words for c in w}
+        for i in range(len(words)-1):
+            w1, w2 = words[i], words[i+1]
+            minLen = min(len(w1), len(w2))
+            if len(w1) > len(w2) and w1[:minLen] == w2[:minLen]: 
+                return ""
+            for j in range(minLen):
+                if w1[j] != w2[j]:
+                    g[w1[j]].add(w2[j])
+                    break
+        visit = {} # False =visited, true = visited and in current path
+        res = []
+        def dfs(c):
+            if c in visit:
+                return visit[c]
+            visit[c] = True
+            for nei in g[c]:
+                if dfs(nei):
+                    return True
+            visit[c] = False
+            res.append(c)
+        for c in g:
+            if dfs(c):
+                return ""
+        res.reverse()
+        return "".join(res)
