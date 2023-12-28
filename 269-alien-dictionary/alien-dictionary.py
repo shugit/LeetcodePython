8b546b21bc4a7e29a381ffada46c2c84652519f5
Shuwen Zhou <jacobc3@gmail.com>
2023-12-28 15:09:12 -0800

    Time: 73 ms (5.62%) | Memory: 17.4 MB (7.13%) - LeetSync

--- 269-alien-dictionary/alien-dictionary.py
+++ 269-alien-dictionary/alien-dictionary.py
@@ -1,28 +1,35 @@
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
-        g = {c:set() for w in words for c in w}
-        for i in range(len(words)-1):
-            w1, w2 = words[i], words[i+1]
-            minLen = min(len(w1), len(w2))
-            if len(w1) > len(w2) and w1[:minLen] == w2[:minLen]: 
-                return ""
-            for j in range(minLen):
-                if w1[j] != w2[j]:
-                    g[w1[j]].add(w2[j])
+        g = defaultdict(set)
+        in_degree = Counter({c:0 for w in words for c in w})
+        for w1, w2 in zip(words, words[1:]):
+            for c1, c2 in zip(w1, w2):
+                if c1 != c2:
+                    if c2 not in g[c1]:
+                        g[c1].add(c2)
+                        in_degree[c2] += 1
                     break
-        visit = {} # False =visited, true = visited and in current path
+            else:
+                if len(w2) < len(w1):
+                    return ""
         res = []
-        def dfs(c):
-            if c in visit:
-                return visit[c]
-            visit[c] = True
-            for nei in g[c]:
-                if dfs(nei):
-                    return True
-            visit[c] = False
-            res.append(c)
-        for c in g:
-            if dfs(c):
-                return ""
-        res.reverse()
+        q = deque()
+        # print(g)
+        # print(in_degree)
+        for node, count in in_degree.items():
+            print(node,count)
+            if count == 0:
+                q.append(node)
+        while q:
+            node = q.popleft()
+            res.append(node)
+            for nei in g[node]:
+                in_degree[nei] -= 1
+                if in_degree[nei] == 0:
+                    q.append(nei)
+        # print(res)
+        if len(res) < len(in_degree):
+            return ""
         return "".join(res)
+
+
