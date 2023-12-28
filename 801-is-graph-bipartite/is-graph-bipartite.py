e9e2f7c72bb3ebdcb477e57fbdc1f10bb95534ac
Shuwen Zhou <jacobc3@gmail.com>
2023-12-27 16:04:41 -0800

    Time: 222 ms (7.02%) | Memory: 17.7 MB (6.43%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -5,11 +5,8 @@ class Solution:
             if i not in visited:
                 q = deque([i])
                 visited[i] = 0
-                for n in ns:
-                    q.append(n)
-                    visited[n] = 1
                 while q:
-                    cur = q.pop()
+                    cur = q.popleft()
                     for nb in graph[cur]:
                         if nb not in visited:
                             q.append(nb)
