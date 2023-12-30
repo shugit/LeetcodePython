162fc5a910f9e3ff86ed2afcbacbf2632725bae2
Shuwen Zhou <jacobc3@gmail.com>
2023-12-30 14:29:09 -0800

    Time: 34 ms (83.77%) | Memory: 17.3 MB (14.20%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,7 +1,5 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        if len(s) == 0:
-            return True
         stack = []
         pairs = {'(':')', '{':'}', '[':']'}
         for c in s:
