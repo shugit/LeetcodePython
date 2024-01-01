9529c61a976b25a7eabb1f18bb8a6e4830ccc758
Shuwen Zhou <jacobc3@gmail.com>
2024-01-01 14:46:56 -0800

    Time: 281 ms (85.88%) | Memory: 33.3 MB (97.03%) - LeetSync

--- 148-sort-list/sort-list.py
+++ 148-sort-list/sort-list.py
@@ -5,6 +5,9 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return self.sol2(head)
+
+    def sol1(self, head):
         if not head:
             return None
         q = []
@@ -17,4 +20,31 @@ class Solution:
             q[i-1][1].next = q[i][1]
         q[len(q)-1][1].next = None
         return q[0][1]
+
+    def sol2(self, head):
+        def mergeSort(node):
+            # print(node)
+            if not node or not node.next:
+                return node
+            slow, fast = node, node.next
+            while fast and fast.next:
+                slow, fast = slow.next, fast.next.next
+            mid = slow.next
+            slow.next = None
+            left = mergeSort(node)
+            right = mergeSort(mid)
+            # print(left, right)
+            dummy = ListNode(0)
+            curr = dummy
+            while left and right:
+                if left.val < right.val:
+                    curr.next = left
+                    left = left.next
+                else:
+                    curr.next = right
+                    right = right.next
+                curr = curr.next
+            curr.next = left or right
+            return dummy.next
+        return mergeSort(head)
         
