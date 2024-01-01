3efc965ba491234267b26cc9ab286e2de636ab0f
Shuwen Zhou <jacobc3@gmail.com>
2023-12-31 21:49:25 -0800

    Time: 51 ms (34.81%) | Memory: 17.7 MB (6.39%) - LeetSync

---
+++ 415-add-strings/add-strings.py
@@ -0,0 +1,22 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        if len(num1) < len(num2):
+            num1, num2 = num2, num1
+        res = []
+        carry = 0
+        i, j = len(num1)-1, len(num2)-1
+        while j >= 0:
+            add = carry + int(num1[i]) + int(num2[j])
+            res.append(add % 10)
+            carry = add // 10
+            i -= 1
+            j -= 1
+        while i >= 0 :
+            add = carry + int(num1[i])
+            res.append(add % 10)
+            carry = add//10
+            i -= 1
+        if carry == 1:
+            res.append(carry)
+        res.reverse()
+        return "".join(map(str,res))
