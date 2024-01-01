6c46786e57e7613dd650858a6707a9cd2ccbccb2
Shuwen Zhou <jacobc3@gmail.com>
2023-12-31 18:22:53 -0800

    Time: 219 ms (9.90%) | Memory: 17.6 MB (13.65%) - LeetSync

--- 265-paint-house-ii/paint-house-ii.py
+++ 265-paint-house-ii/paint-house-ii.py
@@ -5,11 +5,11 @@ class Solution:
         m, n = len(costs), len(costs[0])
         dp = [[0]*n for _ in range(m)]
         dp[0] = costs[0]
-        print(dp)
+        # print(dp)
         for i in range(1, m):
             for j in range(0, n):
                 m1 = min(dp[i-1][:j]) if j > 0 else inf
                 m2 = min(dp[i-1][j+1:]) if j + 1 < n else inf
                 dp[i][j] = costs[i][j] + min(m1, m2)
-            print(dp[i])
+            # print(dp[i])
         return min(dp[m-1])
