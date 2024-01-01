0b8eebaaf2d21ec3e53598364e1256dff7cc1a0a
Shuwen Zhou <jacobc3@gmail.com>
2023-12-31 18:42:33 -0800

    Time: 99 ms (80.21%) | Memory: 17.3 MB (28.33%) - LeetSync

--- 265-paint-house-ii/paint-house-ii.py
+++ 265-paint-house-ii/paint-house-ii.py
@@ -1,11 +1,21 @@
 class Solution:
     def minCostII(self, costs: List[List[int]]) -> int:
-        m, n = len(costs), len(costs[0])
-        dp = [[0]*n for _ in range(m)]
-        dp[0] = costs[0]
-        for i in range(1, m):
-            for j in range(0, n):
-                m1 = min(dp[i-1][:j]) if j > 0 else inf
-                m2 = min(dp[i-1][j+1:]) if j + 1 < n else inf
-                dp[i][j] = costs[i][j] + min(m1, m2)
-        return min(dp[m-1])
+        min1 = min2 = 0
+        idx1 = idx2 = -1
+        for c in costs:
+            curMin1 = curMin2 = inf
+            curIdx1 = curIdx2 = -1
+            for j in range(0, len(c)):
+                if j == idx1:
+                    curC = c[j] + min2
+                else:
+                    curC = c[j] + min1
+                if curC < curMin1:
+                    curMin1, curMin2 = curC, curMin1
+                    curIdx1, curIdx2 = j, curIdx1
+                elif curC <= curMin2:
+                    curMin2, curIdx2 = curC, j
+            # print(curMin1, curMin2, curIdx1, curIdx2)
+            min1, min2 = curMin1, curMin2
+            idx1, idx2 = curIdx1, curIdx2
+        return min1
