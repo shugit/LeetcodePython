770f8d1c3be3f5ed9d80aa43688fef24f7031f2a
Shuwen Zhou <jacobc3@gmail.com>
2024-01-02 12:07:23 -0800

    Time: 77 ms (5.41%) | Memory: 17.7 MB (12.82%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -2,23 +2,17 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         stack = []
         res = []
-        opened, closed = 0,0
-        def bt():
-            nonlocal opened, closed
+        def bt(opened, closed):
             if opened == closed == n:
                 res.append("".join(stack))
                 return
             if opened < n:
                 stack.append("(")
-                opened += 1
-                bt()
-                opened -= 1
+                bt(opened + 1, closed)
                 stack.pop()
             if closed < n and opened > closed:
                 stack.append(")")
-                closed += 1
-                bt()
-                closed -= 1
+                bt(opened, closed + 1)
                 stack.pop()
-        bt()
+        bt(0, 0)
         return res
