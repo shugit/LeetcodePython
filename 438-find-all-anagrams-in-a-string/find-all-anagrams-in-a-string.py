284500b21e96e9c3db974e4330939c179a79c021
Shuwen Zhou <jacobc3@gmail.com>
2024-01-04 11:42:00 -0800

    Time: 110 ms (59.59%) | Memory: 18.5 MB (16.58%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,5 +1,31 @@
 class Solution:
-    def findAnagrams2(self, s: str, p: str) -> List[int]:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        return self.sol3(s,p)
+    def sol3(self, s, p):
+        need = Counter(p)
+        window = Counter()
+        l, r = 0, 0
+        matched = 0
+        res = []
+        while r < len(s):
+            c = s[r]
+            window[c] += 1
+            if c in need and window[c] == need[c]:
+                matched += 1
+            while matched == len(need):
+                if r - l + 1 == len(p):
+                    res.append(l)
+                d = s[l]
+                if d in need and window[d] == need[d]:
+                    matched -= 1
+                window[d] -= 1
+                l += 1
+            r += 1
+        return res
+
+
+
+    def sol2(self, s, p):
         cp = Counter(p)
         cs = Counter(s[0:len(p)])
         res = []
@@ -11,8 +37,7 @@ class Solution:
             if cp == cs:
                 res.append(i)
         return res
-
-    def findAnagrams(self, s: str, p: str) -> List[int]:
+    def sol1(self, s, p):
         p_counter = Counter(p)
         s_counter = Counter()
         left,right = 0,0
