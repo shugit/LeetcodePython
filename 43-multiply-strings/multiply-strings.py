61361d08a36a5072a53b56f0538901f1d5f125a3
Shuwen Zhou <jacobc3@gmail.com>
2024-01-03 19:16:14 -0800

    Time: 106 ms (36.34%) | Memory: 17.2 MB (18.92%) - LeetSync

--- 43-multiply-strings/multiply-strings.py
+++ 43-multiply-strings/multiply-strings.py
@@ -1,5 +1,22 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
+        return self.sol2(num1, num2)
+
+    def sol2(self, num1, num2):
+        if num1 == "0" or num2 =="0":
+            return "0"
+        res = [0] * (len(num1) + len(num2))
+        for i in range(len(num1)-1, -1, -1):
+            for j in range(len(num2)-1, -1, -1):
+                mul = int(num1[i]) * int(num2[j]) + res[j+i+1]
+                res[j+i+1] = mul % 10
+                res[j+i] += mul // 10
+        i = 0
+        while i < len(res) and res[i] == 0:
+            i += 1
+        return "".join(map(str, res[i:]))
+
+    def sol1(self, num1, num2):
         if num1 == "0" or num2 == "0":
             return "0"
         num1 = num1[::-1]
