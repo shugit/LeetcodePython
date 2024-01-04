79b029b30bb1e54e953f5be02f8ab89117f69b8b
Shuwen Zhou <jacobc3@gmail.com>
2024-01-04 11:48:22 -0800

    Time: 73 ms (34.11%) | Memory: 17.4 MB (19.88%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,5 +1,51 @@
 class Solution:
-    def lengthOfLongestSubstring2(self, s: str) -> int:
+
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        return self.sol4(s)
+    def sol4(self, s):
+        if not s:
+            return 0
+        window = Counter()
+        maxi = -inf
+        l, r = 0, 0
+        while r < len(s):
+            c = s[r]
+            window[c] += 1
+            while window[c] > 1:
+                d = s[l]
+                window[d] -= 1
+                l += 1
+            maxi = max(r-l+1, maxi)
+            r += 1
+        return maxi
+
+
+    def sol1(self, s):
+        h = set()
+        l = 0
+        res = 0
+        for r in range(len(s)):
+            while s[r] in h:
+                h.remove(s[l])
+                l += 1
+            h.add(s[r])
+            res = max(res, r-l+ 1)
+        return res
+
+    def sol3(self, s: str) -> int:
+            c = Counter()
+            left = 0
+            res = 0
+            for right in range(0, len(s)):
+                c[s[right]] += 1
+                while c[s[right]] > 1:
+                    c[s[left]] -= 1
+                    left += 1
+                res = max(res, right - left + 1)
+            return res
+
+    def sol2(self, s: str) -> int:
         if len(s) == 0:
             return 0
         res = float("-inf")
@@ -15,27 +61,3 @@ class Solution:
                     h[s[l]] -= 1
                     l += 1
         return res
-
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        h = set()
-        l = 0
-        res = 0
-        for r in range(len(s)):
-            while s[r] in h:
-                h.remove(s[l])
-                l += 1
-            h.add(s[r])
-            res = max(res, r-l+ 1)
-        return res
-
-    def lengthOfLongestSubstring3(self, s: str) -> int:
-            c = Counter()
-            left = 0
-            res = 0
-            for right in range(0, len(s)):
-                c[s[right]] += 1
-                while c[s[right]] > 1:
-                    c[s[left]] -= 1
-                    left += 1
-                res = max(res, right - left + 1)
-            return res
