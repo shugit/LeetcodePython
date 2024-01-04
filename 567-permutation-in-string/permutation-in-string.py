99366811defbcbee4f06691b7b1e976141b2f3d0
Shuwen Zhou <jacobc3@gmail.com>
2024-01-04 11:07:19 -0800

    Time: 69 ms (64.78%) | Memory: 17.7 MB (5.56%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,13 +1,37 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        return self.sol3(s1,s2)
+
+    def sol3(self, s1,s2):
+        if len(s1) > len(s2):
+            return False
+        need = Counter(s1)
+        window = Counter()
+        l, r = 0, 0
+        matched = 0
+        while r < len(s2):
+            c = s2[r]
+            window[c] += 1
+            if c in need and window[c] == need[c]:
+                matched += 1
+            while matched == len(need):
+                if len(s1) == r - l + 1:
+                    return True
+                d = s2[l]
+                if d in need and window[d] == need[d]:
+                    matched -= 1
+                window[d] -= 1
+                l += 1
+            r += 1
+        return False
+
+
+    def sol2(self, s1, s2):
         if len(s1) > len(s2):
             return False
         h1 = Counter(s1)
-        h2 = Counter()
+        h2 = Counter(s2[:len(s1)])
         i = 0
-        n = len(s1)
-        for i in range(0, n):
-            h2[s2[i]] += 1
         if h1 == h2:
             return True
         for i in range(1, len(s2) - len(s1) + 1):
