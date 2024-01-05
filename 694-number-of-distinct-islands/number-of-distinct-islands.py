420ecf7db8939d3231cf854c01af2c8eb2db9416
Shuwen Zhou <jacobc3@gmail.com>
2024-01-05 12:49:55 -0800

    Time: 235 ms (17.15%) | Memory: 19.8 MB (31.09%) - LeetSync

--- 694-number-of-distinct-islands/number-of-distinct-islands.py
+++ 694-number-of-distinct-islands/number-of-distinct-islands.py
@@ -4,6 +4,7 @@ class Solution:
         m, n = len(grid), len(grid[0])
         def dfs(i, j, pi, pj):
             path.append([pi,pj])
+            print(i,j,pi,pj)
             for di, dj in [[1,0], [-1,0], [0,1], [0,-1]]:
                 ni, nj = i + di, j + dj
                 if 0 <= ni < m and 0 <= nj < n and grid[ni][nj] == 1:
@@ -14,8 +15,9 @@ class Solution:
             for j in range(n):
                 if grid[i][j] == 1:
                     path = []
+                    grid[i][j] = 0
                     dfs(i, j, 0, 0)
-                    # print(i,j, path)
+                    print(i,j, path)
                     res.add(" ".join(["".join(map(str, row)) for row in path]))
         # print(res)
         return len(res)
