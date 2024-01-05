442cccd0e6b8de3d80121c4b8f60310a1f0a6db7
Shuwen Zhou <jacobc3@gmail.com>
2024-01-05 09:39:02 -0800

    Time: 51 ms (67.00%) | Memory: 17.3 MB (26.35%) - LeetSync

---
+++ 787-sliding-puzzle/sliding-puzzle.py
@@ -0,0 +1,32 @@
+class Solution:
+    def slidingPuzzle(self, board: List[List[int]]) -> int:
+        j, i = -1, -1
+        for ri in range(len(board)):
+            for rj in range(len(board[ri])):
+                if board[ri][rj] == 0:
+                    i,j = ri, rj
+                    break
+        # print(i,j, board[i][j])
+        q = deque([(i, j, board)])
+        # print("".join(["".join(map(str,row)) for row in board]))
+        visited = set("".join(["".join(str(row)) for row in board]))
+        step = 0
+        while q:
+            for _ in range(len(q)):
+                (i, j, board) = q.popleft()
+                if board == [[1,2,3],[4,5,0]]:
+                    return step
+                for di,dj in [[1,0],[-1,0],[0,1],[0,-1]]:
+                    ni, nj =  i + di, j + dj
+                    if 0 <= ni < 2 and 0 <= nj < 3 :
+                        newBoard = [row[:] for row in board]
+                        newBoard[i][j], newBoard[ni][nj] = newBoard[ni][nj], newBoard[i][j]
+                        boardStr = "".join(["".join(map(str,row)) for row in newBoard])
+                        if boardStr not in visited:
+                            visited.add(boardStr)
+                            q.append([ni, nj, newBoard]) 
+            step += 1
+        return -1
+        
+                
+                
