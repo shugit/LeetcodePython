e01ba13fa78941c579181d43593bf672e534b059
Shuwen Zhou <jacobc3@gmail.com>
2024-01-06 13:46:30 -0800

    Time: 161 ms (95.45%) | Memory: 20.4 MB (88.99%) - LeetSync

--- 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.py
+++ 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.py
@@ -1,10 +1,23 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def nextLargerNodes(self, head: Optional[ListNode]) -> List[int]:
+        return self.sol2(head)
+
+    def sol2(self,head):
+        arr = []
+        while head:
+            arr.append(head.val)
+            head = head.next
+        stack = []
+        res = [0] * len(arr)
+        for i in range(len(arr)-1, -1, -1):
+            while stack and stack[-1] <= arr[i]:
+                stack.pop()
+            res[i] = stack[-1] if stack else 0
+            stack.append(arr[i])
+        return res
+
+
+    def sol1(self, head):
         node = head
         stack = []
         res = []
