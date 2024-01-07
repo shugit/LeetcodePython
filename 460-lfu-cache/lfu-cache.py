a13a2ac7a868525c29c046661479da96471f6eb6
Shuwen Zhou <jacobc3@gmail.com>
2024-01-07 13:43:56 -0800

    Time: 568 ms (96.22%) | Memory: 83 MB (11.97%) - LeetSync

--- 460-lfu-cache/lfu-cache.py
+++ 460-lfu-cache/lfu-cache.py
@@ -1,5 +1,42 @@
 import heapq
+
 class LFUCache:
+    def __init__(self, capacity):
+        self.keyToVal = {} #key: (freq, value)
+        self.freqs = defaultdict(OrderedDict)
+        self.minFreq = 0
+        self.cap = capacity
+
+    def get(self, key) -> int:
+        if key not in self.keyToVal:
+            return -1
+        self.update(key)
+        return self.keyToVal[key][1]
+
+    def put(self, key, value) -> None:
+        if key not in self.keyToVal and self.cap == len(self.keyToVal):
+            toDelKey = self.freqs[self.minFreq].popitem(False)
+            del self.keyToVal[toDelKey[0]]
+        if key in self.keyToVal:
+            self.keyToVal[key][1] = value
+            self.update(key)
+            self.minFreq = min(self.minFreq, self.keyToVal[key][0])
+        else:
+            self.keyToVal[key] = [1, value]
+            self.freqs[1][key] = None
+            self.minFreq = 1
+            
+
+    def update(self, key):
+        f, value = self.keyToVal[key]
+        self.freqs[f].pop(key)
+        self.freqs[f+1][key] = None
+        if not self.freqs[f] and f == self.minFreq:
+            self.minFreq += 1
+        self.keyToVal[key][0] += 1
+        
+
+class LFUCache2:
     def __init__(self, capacity: int):
         self.keyToVal = {}
         self.keyToFreq = {}
