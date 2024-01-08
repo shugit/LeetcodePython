786347c78165f77166bd941cc4f2e8777131a52e
Shuwen Zhou <jacobc3@gmail.com>
2024-01-07 16:10:37 -0800

    Time: 38 ms (94.17%) | Memory: 18 MB (17.43%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.py
+++ 32-longest-valid-parentheses/longest-valid-parentheses.py
@@ -7,12 +7,9 @@ class Solution:
         for i, c in enumerate(s):
             if c == "(":
                 stack.append(i)
-                # dp[i] = 0
             else:
                 if stack:
                     leftIdx = stack.pop()
                     size = i - leftIdx + 1 + dp[leftIdx-1]
                     dp[i] = size
-                else:
-                    dp[i] = 0
         return max(dp)
