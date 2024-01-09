589bde040417e82b6f89c4148e1fa310308f5cf3
Shuwen Zhou <jacobc3@gmail.com>
2024-01-08 16:39:11 -0800

    Time: 1090 ms (29.12%) | Memory: 0B (100.00%) - LeetSync

--- 597-friend-requests-i-overall-acceptance-rate/friend-requests-i-overall-acceptance-rate.sql
+++ 597-friend-requests-i-overall-acceptance-rate/friend-requests-i-overall-acceptance-rate.sql
@@ -1,11 +1,11 @@
 SELECT COALESCE(round(a.total/b.total,2),0) as accept_rate
 FROM (
-    SELECT COALESCE(count(*),0) as total
+    SELECT count(*) as total
     FROM (SELECT * FROM RequestAccepted
     GROUP BY requester_id, accepter_id) m
 ) a
 JOIN (
-    SELECT COALESCE(count(*),0) as total
+    SELECT count(*) as total
     FROM ( SELECT *
     FROM FriendRequest
     GROUP BY sender_id, send_to_id) k
