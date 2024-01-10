da2a61842502035271a2605f4e4c1e590d2056f5
Shuwen Zhou <jacobc3@gmail.com>
2024-01-10 14:42:40 -0800

    Time: 991 ms (56.02%) | Memory: 45.1 MB (10.59%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,5 +1,26 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        return self.sol2(coins, amount)
+    def sol2(self, coins, amount):
+        memo = {0:0}
+        def dp(n):
+            if n in memo:
+                return memo[n]
+            if n < 0:
+                return -1
+            res = inf
+            for coin in coins:
+                sub = dp(n-coin)
+                if sub == -1:
+                    continue
+                res = min(res, sub+1)
+            memo[n] = res if res != inf else -1
+            return memo[n]
+        return dp(amount)
+
+
+
+    def sol1(self, coins, amount):
         n = len(coins)
         dp =  [float("inf") for _ in range(amount + 1)]
         
