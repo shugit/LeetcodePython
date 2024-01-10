eedc5b2f484dbc7b2c02f96b974423b4fe1a041c
Shuwen Zhou <jacobc3@gmail.com>
2024-01-10 14:36:24 -0800

    Time: 34 ms (85.20%) | Memory: 17.2 MB (33.91%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,6 +1,20 @@
 class Solution:
     def fib(self, n: int) -> int:
-        return self.sol2(n)
+        return self.sol3(n)
+    def sol3(self, n):
+        if n == 1: 
+            return 1
+        elif n == 0:
+            return 0
+        d1, d2 = 1, 0
+        for i in range(2, n+1):
+            n = d1 + d2
+            d2 = d1
+            d1 = n
+        return d1
+
+
+
     def sol2(self, n):
         memo = {0:0, 1:1}
         def dp(n):
