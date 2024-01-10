02d0ac06429355d96f9516a08a6fee33a784372b
Shuwen Zhou <jacobc3@gmail.com>
2024-01-10 15:06:51 -0800

    Time: 1833 ms (36.77%) | Memory: 17.7 MB (52.58%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,15 +1,17 @@
 class Solution:
-    def lengthOfLIS2(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp = [1 for _ in range(n)]
-        for i in range(n,-1,-1):
-            for j in range(i+1, n):
-                if nums[i] < nums[j]:
-                    dp[i] = max(dp[i], dp[j] + 1)
-        # print(dp)
-        return max(dp)  
-
     def lengthOfLIS(self, nums: List[int]) -> int:
+        return self.sol3(nums)
+    def sol3(self, nums):
+        dp = [1] * len(nums)
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
+
+
+
+    def sol1(self, nums):
         dp = []
         for num in nums:
             i = bisect_left(dp, num)
@@ -18,3 +20,12 @@ class Solution:
             else:
                 dp[i] = num
         return len(dp)
+    def sol2(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [1 for _ in range(n)]
+        for i in range(n,-1,-1):
+            for j in range(i+1, n):
+                if nums[i] < nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        # print(dp)
+        return max(dp)  
