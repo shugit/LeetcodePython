0f64f7cb029f8fd8c73582fad8c7bf4eaeb17b42
Shuwen Zhou <jacobc3@gmail.com>
2024-01-11 12:27:00 -0800

    Time: 159 ms (63.74%) | Memory: 18.1 MB (5.49%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -1,5 +1,31 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
+        return self.sol_dfs(graph)
+    def sol_dfs(self, graph: List[List[int]]) -> bool:
+        color = {}
+        def dfs(node):
+            for nei in graph[node]:
+                if nei not in color:
+                    color[nei] = -color[node]
+                    if not dfs(nei):
+                        return False
+                else:
+                    if color[nei] == color[node]:
+                        return False
+            return True
+
+        for node, _ in enumerate(graph):
+            if node not in color:
+                color[node] = 1
+                if not dfs(node):
+                    return False
+        return True
+            
+
+
+
+
+    def sol_bfs(self, graph: List[List[int]]) -> bool:
         visited = {}
         for i, ns in enumerate(graph):
             if i not in visited:
