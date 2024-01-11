0ccc8e9e12ed969a66292db69b17f4f283102a48
Shuwen Zhou <jacobc3@gmail.com>
2024-01-11 15:23:54 -0800

    Time: 54 ms (96.93%) | Memory: 21.5 MB (81.52%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -1,5 +1,30 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
+        return self.sol_s(s,t)
+    def sol_s(self, s: str, t: str) -> int:
+        if len(s) < len(t):
+            return 0
+        def dp(i,j):
+            if j == len(t):
+                return 1
+            if len(s) - i < len(t) - j:
+                return 0
+            if (i,j) in memo:
+                return memo[(i,j)]
+            res = 0
+            if s[i] == t[j]:
+                res += dp(i+1, j+1) + dp(i+1, j)
+            else:
+                res += dp(i+1, j)
+            memo[(i,j)] = res
+            return res
+        memo = {}
+        dp(0,0)
+        return memo[(0,0)] 
+
+
+
+    def sol_t(s, t):
         if len(s) < len(t):
             return 0
         memo = {}
