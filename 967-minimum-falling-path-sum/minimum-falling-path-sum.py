ef99c0e1449b4b51b838edba26ea9814be03a221
Shuwen Zhou <jacobc3@gmail.com>
2024-01-10 19:06:50 -0800

    Time: 127 ms (74.06%) | Memory: 19.6 MB (21.40%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -1,5 +1,26 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        return self.topDown(matrix)
+
+    def topDown(self, matrix):
+        n = len(matrix)
+        memo = [[inf]*n for _ in range(n)]
+        def dp(i, j):
+            if not (0 <= i < n) or not (0 <= j < n):
+                return inf
+            if i == 0:
+                memo[i][j] = matrix[i][j]
+                return memo[i][j]
+            if memo[i][j] != inf:
+                return memo[i][j]
+            memo[i][j] = matrix[i][j] + min(dp(i-1, j-1), dp(i-1, j), dp(i-1, j+1))
+            return memo[i][j]
+        for j in range(n):
+            dp(n-1, j)
+        print(memo)
+        return min(memo[n-1])
+
+    def downTop(self, matrix):
         n = len(matrix)
         dp = [x for x in matrix[0]]
         for i in range(1, n):
