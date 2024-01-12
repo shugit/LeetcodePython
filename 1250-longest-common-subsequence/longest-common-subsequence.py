7aeb09d99904d4e52f02e4cda16b6e791e672396
Shuwen Zhou <jacobc3@gmail.com>
2024-01-12 12:19:01 -0800

    Time: 520 ms (81.55%) | Memory: 43.4 MB (30.89%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,6 +1,6 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        return self.topDown(text1, text2)
+        return self.downTop(text1, text2)
     def topDown(self, text1: str, text2: str) -> int:
         memo = {}
         def dp(i,j):
@@ -13,22 +13,20 @@ class Solution:
                 return memo[(i, j)]
             else:
                 res =  max(dp(i+1, j),
-                            dp(i, j+1))
+                            dp(i, j+1),
+                            dp(i+1, j+1))
                 memo[(i, j)] = res
                 return memo[(i, j)]
         return dp(0,0)
-
-
 
 
     def downTop(self, text1: str, text2: str) -> int:
         m = len(text1)
         n = len(text2)
         dp = [ [0 for _ in range(n+1) ] for i in range(m + 1)]
-        
         for i in range(1, m + 1):
             for j in range(1, n + 1):
-                if text1[i-1] == text2[j]:
+                if text1[i-1] == text2[j-1]:
                     dp[i][j] = 1 + dp[i-1][j-1]
                 else:
                     dp[i][j] = max(dp[i][j-1], dp[i-1][j])
