0dc0ea1db0b3da7402c5de5da48f62bf53eeadac
Shuwen Zhou <jacobc3@gmail.com>
2024-01-12 14:51:14 -0800

    Time: 698 ms (14.92%) | Memory: 58.5 MB (16.10%) - LeetSync

---
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
@@ -0,0 +1,17 @@
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        jobs = [(startTime[i], endTime[i], profit[i]) for i in range(len(startTime))]
+        jobs.sort(key=lambda x:x[0])
+        startTime.sort()
+        memo = {}
+        print(jobs)
+        def dfs(i):
+            if i >= len(startTime):
+                return 0
+            if i in memo:
+                return memo[i]
+            index = bisect_left(startTime, jobs[i][1])
+            res = max(jobs[i][2] + dfs(index), dfs(i+1))
+            memo[i] = res
+            return memo[i]
+        return dfs(0)
