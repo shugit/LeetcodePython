99ddb76dbf2d2122284f52ff0abd8c6323c4db65
Shuwen Zhou <jacobc3@gmail.com>
2024-01-12 12:30:15 -0800

    Time: 159 ms (90.91%) | Memory: 21.3 MB (21.84%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.py
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.py
@@ -1,6 +1,6 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        return self.topDown(word1, word2)
+        return self.downTop(word1, word2)
     def topDown(self, word1: str, word2: str) -> int:
         memo = {}
         def dp(i, j):
@@ -20,4 +20,15 @@ class Solution:
 
 
     def downTop(self, word1: str, word2: str) -> int:
-        return 0
+        dp = [[0] * (len(word2)+1) for _ in range(len(word1) + 1)]
+        for i in range(1, len(word1) + 1):
+            dp[i][0] = i
+        for j in range(1, len(word2) + 1):
+            dp[0][j] = j
+        for i in range(1, len(word1)+1):
+            for j in range(1, len(word2) + 1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + 1
+        return dp[len(word1)][len(word2)]
