6e6ca735540fbfd88d50563ad43063cc181027d2
Shuwen Zhou <jacobc3@gmail.com>
2024-01-12 09:57:17 -0800

    Time: 44 ms (58.10%) | Memory: 17.7 MB (8.90%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,18 +1,17 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         return self.topDown(s, wordDict)
+        return self.downTop(s, wordDict)
 
     def downTop(self, s: str, wordDict: List[str]) -> bool:
+        words = set(wordDict)
         n = len(s)
         dp = [False for _ in range(n+1)]
         dp[0] = True
         for i in range(1, n+1):
-            for j in range(0,i):
-                cur = s[j:i] #实际取到的是j~i-1，因为i从1开始，j从0开始，所以正好
-                dp[i] = dp[j] and cur in wordDict
-                if dp[i] is True:
-                    break
-
+            for j in range(0, i):
+                if s[j: i] in words:
+                    dp[i-1] = True
         return dp[n]
 
     def topDown(self, s: str, wordDict: List[str]) -> bool:
