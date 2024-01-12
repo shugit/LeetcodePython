b1a20835fceb64418dcda0c8b406071c9c7a390f
Shuwen Zhou <jacobc3@gmail.com>
2024-01-12 12:00:35 -0800

    Time: 597 ms (53.35%) | Memory: 112.6 MB (6.58%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,5 +1,22 @@
 class Solution:
-    def maxSubArray_dp(self, nums: List[int]) -> int:
+    def maxSubArray(self, nums: List[int]) -> int:
+        return self.topDown(nums)
+    def topDown(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        memo = {}
+        def dp(i):
+            if i == -1:
+                return 0
+            pre = dp(i-1)
+            memo[i] = max(pre + nums[i], nums[i])
+            return memo[i]
+        dp(len(nums)-1)
+        return max(memo.values())
+
+
+
+    def downtop(self, nums):
         n = len(nums)
         dp = {}
         dp[0] = nums[0]
@@ -7,7 +24,7 @@ class Solution:
             dp[i] = max(dp[i-1]+nums[i], nums[i])
         return max(dp.values())
 
-    def maxSubArray(self, nums: List[int]) -> int:
+    def slindingwindow(self, nums: List[int]) -> int:
         maxi = float("-inf")
         res = 0
         for num in nums:
