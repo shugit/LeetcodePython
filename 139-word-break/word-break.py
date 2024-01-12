51a2880f0273a99474fb069f66a5f890d94860b5
Shuwen Zhou <jacobc3@gmail.com>
2024-01-12 10:05:46 -0800

    Time: 39 ms (81.95%) | Memory: 17.6 MB (14.80%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,6 +1,6 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        return self.topDown(s, wordDict)
+        # return self.topDown(s, wordDict)
         return self.downTop(s, wordDict)
 
     def downTop(self, s: str, wordDict: List[str]) -> bool:
@@ -8,10 +8,11 @@ class Solution:
         n = len(s)
         dp = [False for _ in range(n+1)]
         dp[0] = True
-        for i in range(1, n+1):
-            for j in range(0, i):
-                if s[j: i] in words:
-                    dp[i-1] = True
+        for i in range(0, n+1):
+            for w in words:
+                if dp[i] == True and s[i:i+len(w)] == w:
+                    dp[i+len(w)] = True
+        # print(dp)
         return dp[n]
 
     def topDown(self, s: str, wordDict: List[str]) -> bool:
