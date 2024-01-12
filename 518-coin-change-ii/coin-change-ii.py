a4dc6e646bc874ec79eb69e559dab371eaa09617
Shuwen Zhou <jacobc3@gmail.com>
2024-01-12 12:50:34 -0800

    Time: 221 ms (58.89%) | Memory: 32.9 MB (40.62%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,5 +1,24 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
+        return self.downTop(amount, coins)
+
+    def downTop(self, amount: int, coins: List[int]) -> int:
+        dp = [[0] * (amount+1) for _ in range(len(coins)+1)]
+        for i in range(len(coins)+1):
+            dp[i][0] = 1
+        for i in range(1, len(coins)+1):
+            for j in range(1, amount+1):
+                if j-coins[i-1] >= 0:
+                    dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j]
+        return dp[len(coins)][amount]
+
+
+
+
+
+    def sol1(self, amount: int, coins: List[int]) -> int:
         n = len(coins)
         dp = [ [0 for _ in range(amount + 1)] for _ in range(n + 1) ]
       
@@ -12,5 +31,15 @@ class Solution:
                     dp[i][j] = dp[i-1][j]
                 else:
                     dp[i][j] = dp[i][j - cur] + dp[i-1][j]
-        # print(dp)
+        print(dp)
         return dp[n][amount]
+
+    def sol2(self, amount: int, coins: List[int]) -> int:
+        dp = [0 for _ in range(amount + 1)]
+        dp[0] = 1
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] += dp[i - coin]
+            print(dp)
+
+        return dp[amount]
