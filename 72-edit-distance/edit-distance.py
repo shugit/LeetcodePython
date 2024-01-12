62efae4e49aaaa29ee169ff23a944be004bf4b1f
Shuwen Zhou <jacobc3@gmail.com>
2024-01-12 11:29:24 -0800

    Time: 65 ms (98.36%) | Memory: 19.2 MB (82.46%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -1,5 +1,7 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
+        return self.topDown(word1, word2)
+    def sol1(self, word1: str, word2: str) -> int:
         m = len(word1)
         n = len(word2)
         dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
@@ -17,3 +19,39 @@ class Solution:
                     dp[i][j] = min(dp[i-1][j-1],dp[i-1][j], dp[i][j-1]) + 1
         # print(dp)
         return dp[m][n]
+
+    def bruteforce(self, s1: str, s2: str) -> int:
+        def recur(i, j):
+            if i == -1:
+                return j+1
+            if j == -1:
+                return i+1
+            if s1[i] == s2[j]:
+                return recur(i-1, j-1)
+            return 1 + min(
+                recur(i, j-1),
+                recur(i-1, j),
+                recur(i-1, j-1)
+            )
+        return recur(len(s1)-1, len(s2)-1)
+
+    def topDown(self, word1: str, word2: str) -> int:
+        memo = {}
+        def dp(i, j):
+            if i == -1:
+                return j + 1
+            if j == -1:
+                return i + 1
+            if (i,j) in memo:
+                return memo[(i,j)]
+            if word1[i] == word2[j]:
+                memo[(i,j)] = dp(i-1, j-1)
+                return memo[(i,j)]
+            else:
+                memo[(i,j)] = 1+ min(
+                    dp(i-1, j),
+                    dp(i, j-1),
+                    dp(i-1, j-1)
+                )
+                return memo[(i,j)]
+        return dp(len(word1)-1, len(word2)-1)
