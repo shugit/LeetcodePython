d78f0520142ec36885987b5ce7b97f9989a9bb96
Shuwen Zhou <jacobc3@gmail.com>
2024-03-10 22:58:01 -0700

    Time: 39 ms (93.35%) | Memory: 16.7 MB (89.26%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,20 +1,14 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return 0
-        left = 0
-        right = len(nums) - 1
-        while left <= right:
-            mid = left + (right -left)  //2
-            if mid == 0 and nums[mid+1] < nums[mid]: #leftmost element
-                return mid
-            elif mid == len(nums) - 1 and nums[mid-1] < nums[mid]: #rightmost element
-                return mid
-            elif nums[mid-1] < nums[mid] and nums[mid+1] < nums[mid]: # mid is peak
-                return mid
-            elif nums[mid + 1] > nums[mid]: # mid < right
-                left = mid + 1
-            else: # mid < left
-                right = mid - 1
-        # return left
-            
+        l, r = 0, len(nums) - 1
+        # points converge to a peak
+        while l < r:
+            mid = (l + r) // 2
+            # case: descending slope, peak to left
+            if nums[mid] > nums[mid + 1]:
+                r = mid
+            # case: ascending slope, peak to right
+            else:
+                l = mid + 1
+        return l
+
