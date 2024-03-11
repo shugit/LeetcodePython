def579ee5584c9f61f423830c290ec86169cbe44
Shuwen Zhou <jacobc3@gmail.com>
2024-03-10 22:36:41 -0700

    Time: 33 ms (75.91%) | Memory: 16.7 MB (18.98%) - LeetSync

---
+++ 158-read-n-characters-given-read4-ii-call-multiple-times/read-n-characters-given-read4-ii-call-multiple-times.py
@@ -0,0 +1,32 @@
+# The read4 API is already defined for you.
+# def read4(buf4: List[str]) -> int:
+
+class Solution:
+    def __init__(self):
+        self.myBuf = []
+        self.ended = False
+
+    def read(self, buf: List[str], n: int) -> int:
+        while n > len(self.myBuf) and not self.ended:
+            buf4 = [''] * 4
+            res = read4(buf4)
+            if res == 0:
+                self.ended = True
+            else:
+                self.myBuf += buf4[:res]
+                print(buf4, res, self.myBuf)
+        print(n, self.myBuf)
+        if n > len(self.myBuf):
+            i = 0
+            while i < len(buf) and buf[i] != '':
+                buf[i] = ''
+                i += 1
+            for i in range(len(self.myBuf)):
+                buf[i] = self.myBuf[i]
+            self.myBuf = []
+            return len(buf)
+        for i in range(n):
+            buf[i] = self.myBuf[i]
+        self.myBuf = self.myBuf[n:]
+        print(n,buf[:n], self.myBuf)
+        return n
