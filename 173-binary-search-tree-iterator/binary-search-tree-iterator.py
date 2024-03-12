31a28749a81fd8d406509bd941ac750f4e6445bf
Shuwen Zhou <jacobc3@gmail.com>
2024-03-11 21:45:09 -0700

    Time: 31 ms (100.00%) | Memory: 22.5 MB (25.93%) - LeetSync

--- 173-binary-search-tree-iterator/binary-search-tree-iterator.py
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.py
@@ -3,18 +3,18 @@ class BSTIterator:
         self.stack = deque([])
         self.push_left(root)
     
-    def push_left(self,root):
-        while root:
-            self.stack.append(root)
-            root = root.left
+    def push_left(self, node):
+        while node:
+            self.stack.append(node)
+            node = node.left
 
     def next(self) -> int:
-        root = self.stack.pop()
-        if root.right:
-            self.push_left(root.right)
-        return root.val
+        node = self.stack.pop()
+        if node.right:
+            self.push_left(node.right)
+        return node.val
 
     def hasNext(self) -> bool:
-        if len(self.stack)!=0:
+        if len(self.stack) != 0:
             return True
         return False
