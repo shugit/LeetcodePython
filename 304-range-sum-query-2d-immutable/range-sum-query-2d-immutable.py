80f6324b2ed3d07a6af89a30c11df0ee153e2faf
Shuwen Zhou <jacobc3@gmail.com>
2024-03-14 22:37:20 -0700

    Time: 993 ms (67.71%) | Memory: 27.3 MB (64.68%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -6,7 +6,6 @@ class NumMatrix:
         for i in range(1, m+1):
             for j in range(1, n+1):
                 self.preSum[i][j] = self.preSum[i-1][j] + self.preSum[i][j-1] + matrix[i-1][j-1] - self.preSum[i-1][j-1]
-        # print(self.preSum)
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         return self.preSum[row2+1][col2+1] - self.preSum[row1][col2+1] - self.preSum[row2+1][col1] + self.preSum[row1][col1]
