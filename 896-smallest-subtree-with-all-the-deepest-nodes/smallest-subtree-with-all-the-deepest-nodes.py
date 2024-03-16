4235f9808237fabaccedf5136dadecbf607207b2
Shuwen Zhou <jacobc3@gmail.com>
2024-03-16 14:59:23 -0700

    Time: 42 ms (23.65%) | Memory: 16.7 MB (64.84%) - LeetSync

--- 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.py
+++ 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.py
@@ -1,26 +1,16 @@
 class Solution:
     def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
-        if not root:
-            return root
-        q = deque([root])
-        level = []
-        while q:
-            level = []
-            for _ in range(len(q)):
-                node = q.popleft()
-                level.append(node.val)
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-        def find(node):
+        def find(node, depth):
             if not node:
-                return node
-            if node.val in level:
-                return node
-            left = find(node.left)
-            right = find(node.right)
-            if left and right:
-                return node
-            return left or right
-        return find(root)
+                return None, depth
+            leftLca, leftDepth = find(node.left, depth + 1)
+            rightLca, rightDepth = find(node.right, depth + 1)
+            if leftDepth == rightDepth:
+                return node, leftDepth
+            elif leftDepth > rightDepth:
+                return leftLca, leftDepth
+            else:
+                return rightLca, rightDepth
+
+        lca, _ = find(root, 0)
+        return lca
