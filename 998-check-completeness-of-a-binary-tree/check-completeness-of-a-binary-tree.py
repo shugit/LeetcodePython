2e597c2749b959869a2de14374058930b35c450c
Shuwen Zhou <jacobc3@gmail.com>
2024-03-16 15:16:58 -0700

    Time: 42 ms (24.22%) | Memory: 16.5 MB (94.01%) - LeetSync

--- 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.py
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.py
@@ -3,13 +3,12 @@ class Solution:
         q = deque([root])
         nullFound = False
         while q:
-            for _ in range(len(q)):
-                node = q.popleft()
-                if not node:
-                    nullFound = True
-                else:
-                    if nullFound:
-                        return False
-                    q.append(node.left)
-                    q.append(node.right)
+            node = q.popleft()
+            if not node:
+                nullFound = True
+            else:
+                if nullFound:
+                    return False
+                q.append(node.left)
+                q.append(node.right)
         return True
