8e629b264039c05a4bc08c10433d1c9870d61a29
Shuwen Zhou <jacobc3@gmail.com>
2024-03-16 22:25:38 -0700

    Time: 223 ms (59.63%) | Memory: 23 MB (56.38%) - LeetSync

--- 1059-missing-element-in-sorted-array/missing-element-in-sorted-array.py
+++ 1059-missing-element-in-sorted-array/missing-element-in-sorted-array.py
@@ -4,12 +4,12 @@ class Solution:
         if missed < k:
             return nums[-1] + k - missed
         l, r = 0, len(nums) - 1
-        while l <= r:
+        while l < r:
             mid = l + (r - l) // 2
             if nums[mid] - nums[0] - mid < k:
                 l = mid + 1
             else:
-                r = mid - 1
+                r = mid 
         return nums[0] + l + k - 1
             
         
