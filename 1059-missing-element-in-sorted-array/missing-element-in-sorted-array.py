9aedcac25110bea89cc30de01bcab6caa1db8b59
Shuwen Zhou <jacobc3@gmail.com>
2024-03-16 22:25:06 -0700

    Time: 229 ms (35.33%) | Memory: 23 MB (14.73%) - LeetSync

--- 1059-missing-element-in-sorted-array/missing-element-in-sorted-array.py
+++ 1059-missing-element-in-sorted-array/missing-element-in-sorted-array.py
@@ -1,15 +1,15 @@
 class Solution:
     def missingElement(self, nums: List[int], k: int) -> int:
-        missed= nums[-1] - nums[0] - len(nums) + 1
+        missed = nums[-1] - nums[0] - len(nums) + 1
         if missed < k:
             return nums[-1] + k - missed
         l, r = 0, len(nums) - 1
-        while l < r:
+        while l <= r:
             mid = l + (r - l) // 2
             if nums[mid] - nums[0] - mid < k:
                 l = mid + 1
             else:
-                r = mid
+                r = mid - 1
         return nums[0] + l + k - 1
             
         
