518eb66efe11cfdcfb5d71d276b663d5b59f89c9
Shuwen Zhou <jacobc3@gmail.com>
2024-03-18 20:37:09 -0700

    Time: 186 ms (89.53%) | Memory: 20.4 MB (82.50%) - LeetSync

--- 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.py
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.py
@@ -1,16 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-     def balanceBST(self, root):
+    def balanceBST(self, root: TreeNode) -> TreeNode:
+        path = []
+
+        def inorder(root):
+            if not root: return
+
+            inorder(root.left)
+            path.append(root)
+            inorder(root.right)
+
+        inorder(root)
+        n = len(path)
+
+        def construct(low, high):
+            if low > high: return
+            mid = (low + high) // 2
+
+            node = path[mid]
+            node.left = construct(low, mid - 1)
+            node.right = construct(mid + 1, high)
+
+            return node
+
+        return construct(0, n - 1)
         
-        def dfs(node):
-            if not node: return []
-            return dfs(node.left) + [node.val] + dfs(node.right)
-        ns = dfs(root)
-        
-        def build(l, r):
-            if l > r: return None
-            m = (l + r) // 2
-            root = TreeNode(ns[m])
-            root.left, root.right = build(l, m-1), build(m + 1, r)
-            return root
-        
-        return build(0, len(ns) - 1)
