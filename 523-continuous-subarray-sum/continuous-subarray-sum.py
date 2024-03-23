ac49a96d6bc48392b9e3330827d748f6e4a94a91
Shuwen Zhou <jacobc3@gmail.com>
2024-03-23 13:55:35 -0700

    Time: 823 ms (76.37%) | Memory: 36.3 MB (70.77%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -1,16 +1,14 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        preSum = [0]
-        for n in nums:
-            preSum.append(preSum[-1] + n)
-        valToIndex = {}
-        for i in range(len(preSum)):
-            val = preSum[i] % k
-            if val not in valToIndex:
-                valToIndex[val] = i
-        for i in range(1, len(preSum)):
-            need = preSum[i] % k
-            if need in valToIndex:
-                if i - valToIndex[need] >= 2:
+        h = {0 : -1}
+        curSum = 0
+        for i, n in enumerate(nums):
+             curSum += n
+             r = curSum % k
+             if r in h:
+                dist = i - h[r]
+                if dist > 1:
                     return True
+             else:
+                h[r] = i
         return False
