98891c400cc0afa818dd19c70b708956a6e3dae7
Shuwen Zhou <jacobc3@gmail.com>
2024-03-28 22:31:56 -0700

    Time: 31 ms (99.22%) | Memory: 16.7 MB (7.28%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -2,11 +2,8 @@ class Solution:
     def intToRoman(self, num: int) -> str:
         k = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
         res = ""
-        while num > 0:
-            for n, letter in k:
-                if n <= num:
-                    num -= n
-                    # print(n, letter, num)
-                    res += letter
-                    break
+        for n, letter in k :
+            while n <= num:
+                num -= n
+                res += letter
         return res
