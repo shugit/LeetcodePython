0a1b84da68f413817fc5e1c870a8b14a3d01829b
Shuwen Zhou <jacobc3@gmail.com>
2024-03-28 22:15:05 -0700

    Time: 46 ms (60.46%) | Memory: 16.6 MB (69.99%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -3,9 +3,8 @@ class Solution:
         res = 0
         i = 0
         h = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-        special = {'I':set(['V','X']),'X':set(['L','C']),'C':set(['D','M'])}
         while i < len(s):
-            if s[i] in special and i+1 < len(s) and s[i+1] in special[s[i]]:
+            if i+1 < len(s) and h[s[i]] < h[s[i+1]]:
                 res -= h[s[i]]
             else:
                 res += h[s[i]]
