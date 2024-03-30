a58509e84418e64be30a0c95f70331905ff32b40
Shuwen Zhou <jacobc3@gmail.com>
2024-03-29 23:15:12 -0700

    Time: 45 ms (55.06%) | Memory: 16.8 MB (37.78%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.py
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -1,36 +1,14 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        memo = [[-1] * (len(p)+1) for _ in range(len(s)+1)]
-        def dp(i, j):
-            if memo[i][j] != -1:
-                return memo[i][j]
-            if j == len(p):
-                memo[i][j] = (i == len(s))
-                return memo[i][j]
-            if i == len(s):
-                if (len(p) - j) % 2 == 1:
-                    memo[i][j] = False
-                    return False
-                for k in range(j+1, len(p), 2):
-                    if p[k] != "*":
-                        memo[i][j] = False
-                        return False
-                memo[i][j] = True
-                return memo[i][j]
-            res = False
-            if s[i] == p[j] or p[j] == '.':
-                if j < len(p) - 1 and p[j+1] == "*":
-                    res = dp(i, j + 2) or dp(i + 1, j)
-                else:
-                    res = dp(i + 1, j + 1)
-            else:
-                if j < len(p) - 1 and p[j+1] == "*":
-                    res = dp(i, j + 2)
-                else:
-                    res = False
-            memo[i][j] = res
-            return memo[i][j]
-        dp(0, 0)
-        return memo[0][0]
-
-            
+        dp = defaultdict(lambda: False)
+        dp[(len(s),len(p))] = True
+        for i in range(len(s), -1, -1):
+            for j in range(len(p)-1, -1, -1):
+                if j + 1 < len(p) and p[j+1] == "*":
+                    dp[(i,j)] = dp[(i, j + 2)]
+                    if i < len(s) and s[i] == p[j] or p[j] == ".":
+                        dp[(i,j)] = dp[(i+1,j)] or dp[(i, j)]
+                elif i < len(s) and s[i] == p[j] or p[j] == ".":
+                    dp[(i,j)] = dp[(i+1, j + 1)]
+        return dp[(0,0)]
+        
